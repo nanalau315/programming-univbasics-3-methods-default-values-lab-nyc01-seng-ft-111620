3e7269a2158d578ca9d8f7d9e842bd22ac60f6c2
Wing Nam Lau <nanalau315@hotmail.com>
2020-10-29 23:30:44 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,7 @@
 # "A plate of tofu with broccoli and macaroni."
 
 
-def meal_choice(veg1, veg2, protein = tofu)
+def meal_choice(veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
